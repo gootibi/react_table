c0c3b014c533fe8cf43b6a3c3bfe3d909588c77f
gootibi <gootibi@gmail.com>
2023-12-31 01:46:50 +0100

    Add style Changes to be committed: 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import './App.css';
 import fakeData from './MOCK_DATA.json';
 import * as React from 'react';
+import { useTable } from 'react-table';
 
 function App() {
+
+  const data = React.useMemo(() => fakeData, []);
+  const columns = React.useMemo(() => [
+    {
+      Header: 'ID',
+      accessor: 'id'
+    },
+    {
+      Header: 'First Name',
+      accessor: 'first_name'
+    },
+    {
+      Header: 'Last Name',
+      accessor: 'last_name'
+    },
+    {
+      Header: 'Email',
+      accessor: 'email'
+    },
+    {
+      Header: 'Gender',
+      accessor: 'gender'
+    },
+    {
+      Header: 'University',
+      accessor: 'university'
+    },
+  ]);
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
   return (
     <div className="App">
-      hello world
+      <div className='container'>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps}>
+                {headerGroup.headers.map((column) => (
+                  
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
